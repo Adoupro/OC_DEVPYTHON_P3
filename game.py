b2aee8924b83ac583297cbc8501be2d88c50a103
YAPO-AKO <Adou.YAPO-AKO@FromEarthToLife.net>
2020-07-04 12:19:30 +0200

    Version 1.0

---
+++ game.py
@@ -0,0 +1,98 @@
+"""
+Game.py contain the code in charge to execute the game
+"""
+
+# Import of libraries
+import pygame
+from script import MacGiver, Guardian, Delimiter, Item
+
+# Game initialization
+pygame.init()
+pygame.time.Clock().tick(30) # Refresh every 30 milliseconds only
+pygame.font.init()
+
+# Create Framework object
+velocity = 20
+framework = pygame.image.load('ressource/structures.png')
+delimiter = Delimiter(velocity)
+limit = delimiter.limit
+allowed = delimiter.allowed
+
+# Create MacGiver object
+startX = delimiter.start[0]
+startY = delimiter.start[1]
+macGyver = MacGiver(startX, startY, velocity)
+
+# Create Guardian object
+exitX = delimiter.exit[0]
+exitY = delimiter.exit[1]
+guardian = Guardian(exitX, exitY)
+
+# Create Item object
+items = Item(allowed, macGyver.width, macGyver.heigth)
+
+# Window initialization
+winWidth = 300
+winHeigth = 160
+win = pygame.display.set_mode((winWidth, winHeigth))
+count = 0
+pygame.display.set_caption("Aidez MacGyver ! {}/3".format(count))
+win.blit(framework, (0, 0))
+run = True
+
+# Game execution
+while run:
+    pygame.time.delay(100)
+
+    # Shifting management
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and macGyver.x > macGyver.velocity:
+        macGyver.moveRight(limit)
+    if keys[pygame.K_RIGHT] and macGyver.x < winWidth - macGyver.velocity - macGyver.width:
+        macGyver.moveLeft(limit)
+    if keys[pygame.K_UP] and macGyver.y > macGyver.velocity:
+        macGyver.moveUp(limit)
+    if keys[pygame.K_DOWN] and macGyver.y < winHeigth - macGyver.velocity - macGyver.heigth:
+        macGyver.moveDown(limit)
+
+    # Item management
+    if (macGyver.x, macGyver.y)==items.needlePosition and items.pop['needle']==True:
+        items.pop['needle'] = False
+        count += 1
+    if (macGyver.x, macGyver.y)==items.tubePosition and items.pop['tube']==True:
+        items.pop['tube'] = False
+        count += 1
+    if (macGyver.x, macGyver.y)==items.etherPosition and items.pop['ether']==True:
+        items.pop['ether'] = False
+        count += 1
+
+    # Displaying management
+    pygame.display.set_caption("Aidez MacGyver ! {}/3".format(count))
+    win.blit(framework, (0, 0))
+    win.blit(macGyver.image, (macGyver.x, macGyver.y))
+    win.blit(guardian.image, guardian.position)
+
+    if items.pop['needle']:
+        win.blit(items.needleImage, items.needlePosition)
+    if items.pop['tube']:
+        win.blit(items.tubeImage, items.tubePosition)
+    if items.pop['ether']:
+        win.blit(items.etherImage, items.etherPosition)
+
+    # Game result management
+    if (macGyver.x, macGyver.y)==guardian.position:
+        font = pygame.font.SysFont('Comic Sans MS', 30)
+        if True in items.pop.values():
+            text = font.render(guardian.lose(), False, (255, 255, 255))
+        else:
+            text = font.render(guardian.win(), False, (255, 255, 255))
+        win.blit(text, (0, 0))
+
+    # Game execution management
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
---
+++ script.py
@@ -0,0 +1,115 @@
+import pygame
+import random
+
+class MacGiver:
+    """Give birth to Mac Giver"""
+    def __init__(self, x, y, velocity):
+        self.image = pygame.image.load('ressource/MacGyver.png')
+        self.width = 16
+        self.heigth = 16
+        self.image = pygame.transform.scale(self.image, (self.width, self.heigth))
+        self.x = x
+        self.y = y
+        self.velocity = velocity
+
+    def moveRight(self, limit):
+        #Make a move to the right
+        if (self.x-self.velocity, self.y) not in limit:
+            self.x -= self.velocity
+            return self.x
+
+    def moveLeft(self, limit):
+        #Make a move to the left
+        if (self.x+self.velocity, self.y) not in limit:
+            self.x += self.velocity
+            return self.x
+
+    def moveUp(self, limit):
+        #Make a move to the top
+        if (self.x, self.y-self.velocity) not in limit:
+            self.y -= self.velocity
+            return self.y
+
+    def moveDown(self, limit):
+        #Make a move to the bottom
+        if (self.x, self.y+self.velocity) not in limit:
+            self.y += self.velocity
+            return self.y
+
+class Guardian:
+    """Guardian Interaction"""
+
+    def __init__(self, x, y):
+        self.image = pygame.image.load('ressource/Gardien.png')
+        self.width = 16
+        self.heigth = 16
+        self.image = pygame.transform.scale(self.image, (self.width, self.heigth))
+        self.position = (x, y)
+
+    def win(self):
+        # Return the message when the player win the game
+        return 'You win !'
+
+    def lose(self):
+        # Return the message when the player lose the game
+        return 'You lose !'
+
+class Delimiter:
+    """Delimit the level. Return the allowed and forbidden emplacements."""
+    def __init__(self, velocity):
+
+        self.limit = []
+        self.allowed = []
+
+        with open('framework.txt', 'r') as file:
+            for y, line in enumerate(file):
+
+                # Y emplacement computation
+                if y==0:
+                    y += 1
+                else:
+                    y = y*velocity+1
+
+                for x, value in enumerate(line[:-1]):
+
+                    # X emplacement computation
+                    if x==0:
+                        x += 1
+                    else:
+                        x = x*velocity+1
+
+                    # Get the forbidden and allowed emplacements
+                    if value=='s':
+                        self.start = (x, y)
+                    elif value=='e':
+                        self.exit = (x, y)
+                    elif value=='w' and (x, y):
+                        self.limit.append((x, y))
+                    elif value!='w' and (x, y):
+                        self.allowed.append((x, y))
+
+
+class Item:
+    """Manage the Items population"""
+    def __init__(self, allowed, width, heigth):
+
+        # Needle generation
+        self.needleImage = pygame.image.load('ressource/aiguille.png')
+        self.needleImage = pygame.transform.scale(self.needleImage, (width, heigth))
+        self.needlePosition = random.choice(allowed)
+        allowed.remove(self.needlePosition)
+
+        # Tube generation
+        self.tubeImage = pygame.image.load('ressource/tube_plastique.png')
+        self.tubeImage = pygame.transform.scale(self.tubeImage, (width, heigth))
+        self.tubePosition = random.choice(allowed)
+        allowed.remove(self.tubePosition)
+
+        # Ether generation
+        self.etherImage = pygame.image.load('ressource/ether.png')
+        self.etherImage = pygame.transform.scale(self.etherImage, (width, heigth))
+        self.etherPosition = random.choice(allowed)
+        allowed.remove(self.etherPosition)
+
+        # Population rights
+        self.pop = {'needle':True, 'tube':True, 'ether':True}
